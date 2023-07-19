3cf1dd92a98e51781cb9bd7535d12910cbf1a60b
Kirra Hyde <kirra.hyde@gmail.com>
2023-07-18 20:08:16 -0400

    WIP random word

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,16 @@
+from random import choice
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+    def __init__(self, path):
+        self.file = open(path)
+
+
+    def make_list(self):
+        return self.file.readlines()
+
+    def random(self):
+        return choice(self.make_list())
+
+
+
